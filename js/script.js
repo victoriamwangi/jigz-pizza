3694b8f1ab5bfc3626bd0aeaf76b9c18e447280c
victoria mwangi <victoriamwangi021@gmail.com>
2022-03-19 21:04:55 +0300

    feat:calculate total

--- js/script.js
+++ js/script.js
@@ -2,11 +2,15 @@ function Pizza(size, crust, topping) {
     this.size = size;
     this.crust = crust;
     this.toppings = topping;
-}
-
+};
 var total = function(sizeprice, crustprice, toppinsprice) {
     return (sizeprice + crustprice + toppinsprice);
-}
+
+};
+var price;
+var sizeChosen;
+var sizePrice;
+
 
 
 $(document).ready(function() {
@@ -17,27 +21,60 @@ $(document).ready(function() {
 
     })
     $("#add").click(function() {
-        // var sizeChosen = $("#size :selected").val();
-        var sizeChosen = $("#size :selected").text();
-        var crustChosen = $('#crust :selected').text();
-        var toppingsChosen = $('#toppings :selected').text();
+        sizeChosen = $("#size :selected").text();
+        crustChosen = $('#crust :selected').text();
+        toppingsChosen = $('#toppings :selected').text();
+
+        if (sizeChosen == "Small") {
+            sizePrice = 500;
+        } else if (sizeChosen == "Medium") {
+            sizePrice = 600;
+        } else if (sizeChosen == "Large") {
+            sizePrice = 700;
+            return false;
+        }
+        console.log(sizePrice)
+
+        if (crustChosen == "Crispy") {
+            crustPrice = 200;
+        } else if (crustChosen == "Stuffed") {
+            crustPrice = 250;
+        } else if (crustChosen == "Gluten-free") {
+            crustPrice = 300;
+            return false;
+        }
+        console.log(crustPrice)
+        if (toppingsChosen == "Pepperoni") {
+            topPrice = 200;
+        } else if (toppingsChosen == "Extra Chesse") {
+            topPrice = 300;
+        } else if (toppingsChosen == "Chicken") {
+            topPrice = 200;
+            return false;
+        }
+        console.log(typeof(topPrice))
+        var totalPrice = total(sizePrice, crustPrice, topPrice)
 
         $("#pizza_table").append(
             '<tr>' +
             '<td>' + sizeChosen + '</td>' +
             '<td>' + crustChosen + '</td>' +
             '<td>' + toppingsChosen + '</td>' +
-            '<td>' + '<p>' + "hehe" + '</p>' + '<td>' +
+            '<td>' + totalPrice + '<td>' +
             '</tr>'
-        )
-        $('.submit').show()
+        );
+        $('.submit').show();
 
     });
     $('.submit').click(function() {
         $('#checkout').modal('show');
-        $('.total_div   ').show();
+        // $('modal .total_div').append(
+        //     '<p>' + "hehe" + '</p>' +
+        //     '<p>' + totalPrice + "hehe" + '</p>'
+        // )
+        // $('.total').innerText = totalPrice;
+    });
 
-    })
     $('#checkout_yes').click(function() {
         $('.grand_div').show();
         $('.total_div').hide();
@@ -46,12 +83,10 @@ $(document).ready(function() {
             $('.final').show();
             var location = $("#locale").val();
             $('.final').append(
-                '<p>' + "YOur order will be delivered to" + " " + location + '</p>'
-            )
-        })
-
+                '<p>' + "Your order will be delivered to" + " " + location + '</p>'
+            );
+        });
     });
-
     $('#checkout_no').click(function() {
         $('.total_div').hide();
         $('.grand_div').hide();
